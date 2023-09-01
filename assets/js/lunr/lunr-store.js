26737ebc032f1d6b4353e4fab509dfa008023879
DiogoRibeiro7 <DiogoRibeiro7@users.noreply.github.com>
2023-09-01 21:18:13 +0000

    published by GitHub Actions

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Demystifying Data Science",
+        "excerpt":"Data science is a term that has been gaining a lot of attention in recent years. It is a multidisciplinary field that combines statistics, computer science, and domain expertise to extract insights from data. In this article, we will demystify data science, discuss its benefits, and explore its applications in...","categories": ["Data Science"],
+        "tags": ["Data Science"],
+        "url": "/data%20science/Demystifying-Data-Science/",
+        "teaser": null
+      },{
+        "title": "The Vulnerability of Large Language Models to the Closure of Open-Source Data Platforms",
+        "excerpt":"Decay of traffic in Stack Overflow Ah, my friends, imagine for a moment that we dwell in a universe sculpted by algorithms, vast webs of equations that dance and weave the fabric of our digital cosmos. These ethereal creatures, known to most as Large Language Models, are the loquacious virtuosos...","categories": ["Artificial Intelligence"],
+        "tags": ["LLM"],
+        "url": "/artificial%20intelligence/Large-languague-models/",
+        "teaser": null
+      },{
+        "title": "The Life and Legacy of Paul Erdös",
+        "excerpt":"Paul Erdös When one speaks of Paul Erdős, it’s as if one has whispered the secret name of a mathematical deity, summoning forth the very essence of intellectual curiosity and rigorous thought. Erdős was no mere mathematician, though his contributions would have sufficed to solidify that title. He was, in...","categories": ["Genius"],
+        "tags": ["Paul Erdös","Mathematics"],
+        "url": "/genius/Paul-Erdos/",
+        "teaser": null
+      },{
+        "title": "The Unseen Art of Data Quality: Bridging the Gap Between Collection and Utilization",
+        "excerpt":"The neglect of data design and data quality is a pervasive issue within the data industry, hindering efforts to extract value from data. Although data science is focused on making data useful, data engineering is concerned with making data usable. However, the art of creating good data is often overlooked,...","categories": ["Data Science"],
+        "tags": ["Data Science","Data Engineering","Data Quality"],
+        "url": "/data%20science/data_design/",
+        "teaser": null
+      },{
+        "title": "Navigating the Ethical Landscape of Data Science",
+        "excerpt":"In the age of big data and artificial intelligence, data science has emerged as a transformative force, reshaping industries, driving innovation, and unlocking new frontiers of knowledge. From personalized healthcare to intelligent transportation systems, data science applications are ubiquitous, touching every aspect of our lives. Yet, with great power comes...","categories": ["Data Science"],
+        "tags": ["Data Science","Artificial Intelligence","Machine Learning","Ethics"],
+        "url": "/data%20science/Data_Science/",
+        "teaser": null
+      }]
