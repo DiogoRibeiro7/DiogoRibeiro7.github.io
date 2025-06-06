0cec5aca32e63b7bad2e2556f7f654ba6d4cb07c
Diogo Ribeiro <diogo_dj@hotmail.com>
2025-06-06 15:24:29 +0100

    Use python3 in run_scripts

--- run_scripts.sh
+++ run_scripts.sh
@@ -2,9 +2,9 @@
 
 TARGET="./_posts"
 
-python markdown_file_processor.py --path "$TARGET"
-python fix_frontmatter.py --path "$TARGET"
-python search_code_snippets.py --path "$TARGET"
-# python process_markdown_frontmatter.py --path "$TARGET"
-python rename_files_spaces.py --path "$TARGET"
-python markdown_frontmatter_cleanup.py --path "$TARGET"
+python3 markdown_file_processor.py --path "$TARGET"
+python3 fix_frontmatter.py --path "$TARGET"
+python3 search_code_snippets.py --path "$TARGET"
+# python3 process_markdown_frontmatter.py --path "$TARGET"
+python3 rename_files_spaces.py --path "$TARGET"
+python3 markdown_frontmatter_cleanup.py --path "$TARGET"
